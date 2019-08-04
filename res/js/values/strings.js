7856ae9bffb27a89c96719e3fcf5335aeb9373eb
amirisback <faisalamircs@gmail.com>
2019-08-04 22:49:11 +0700

    add: Skeleton Website

---
+++ res/js/values/strings.js
@@ -0,0 +1,11 @@
+const web_name = "Muhammad Faisal Amir";
+const hoho = "Muhammad Faisal Amir";
+const web_name = "Muhammad Faisal Amir";
+const web_name = "Muhammad Faisal Amir";
+const web_name = "Muhammad Faisal Amir";
+const web_name = "Muhammad Faisal Amir";
+const web_name = "Muhammad Faisal Amir";
+
+function setText(id, value){
+    document.getElementById(id).innerHTML = value
+}
